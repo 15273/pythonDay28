cf31f0abfe685fedf1380195f611a915bb8831a1
merp770g@gmail.com <merp770g@gmail.com>
2022-10-02 22:47:06 +0300

    new changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,23 +9,45 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+    small_brake = SHORT_BREAK_MIN * 60
+    long_brake = LONG_BREAK_MIN * 60
+    work_time = WORK_MIN * 60
+    if reps % 2 == 0:
+        title_label.config(text="Short Brake", fg=PINK)
+        count_down(small_brake)
+    elif reps % 8 == 0:
+        title_label.config(text="Long Brake", fg=PINK)
+        count_down(long_brake)
+    else:
+        title_label.config(text="Work Time", fg=GREEN)
+        count_down(work_time)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def count_down(count):
-    print(count)
-    canvas.itemconfig(timer_text, text=count)
+
+    count_min = math.floor(count / 60)
+    count_second = count % 60
+    if count_second < 10:
+        count_second = f"0{count_second}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_second}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
